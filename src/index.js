102bd6d66a5f7e2cff126fb084e05cf1b89b7251
Math-maz <mazmerox@gmail.com>
2020-03-16 10:19:37 -0300

    Add better description to bot's activity

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ const client = new Client();
 const { messageManager } = require("./controllers/messageController");
 client.on("ready", () => {
   console.log("i'm ready to interact!");
-  client.user.setActivity({ name: "!ranked-check", type: "LISTENING" });
+  client.user.setActivity({
+    name: "!ranked-check and !single-check NICK",
+    type: "LISTENING"
+  });
 });
 
 client.on("message", messageManager);
