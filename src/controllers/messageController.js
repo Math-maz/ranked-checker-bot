862d2aa65e7a46a59e95ad465f06ba042e968ea1
Math-maz <mazmerox@gmail.com>
2020-03-16 10:17:13 -0300

    Remove logs

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -10,7 +10,6 @@ module.exports = {
   messageManager: async msg => {
     const messageArr = msg.content.split(" ");
     const parsedMsg = parseArgs(messageArr);
-    console.log(parsedMsg);
     const command = parsedMsg._[0];
     const nick = parsedMsg._[1] || null;
 
@@ -18,7 +17,6 @@ module.exports = {
       await msg.reply("Performing ranked check on everybody!");
       players.forEach(async playerID => {
         const response = await axios.get(formatLeagueAPIQuery(playerID));
-        // console.log(response.data);
         let soloQStats, flexStats;
         if (response.data[0].queueType == "RANKED_FLEX_SR") {
           flexStats = response.data[0];
@@ -64,13 +62,10 @@ module.exports = {
           const summonerResponse = await axios.get(
             formatSummonerLeagueAPIQuery(nick)
           );
-          console.log(summonerResponse.data);
           const summonerId = summonerResponse.data.id;
-          // console.log(summonerId);
           const rankedStatsResponse = await axios.get(
             formatLeagueAPIQuery(summonerId)
           );
-          console.log(rankedStatsResponse.data);
           let soloQStats, flexStats;
           if (rankedStatsResponse.data[0].queueType == "RANKED_FLEX_SR") {
             flexStats = rankedStatsResponse.data[0];
@@ -118,46 +113,5 @@ module.exports = {
         }
       }
     }
-
-    // if (command === "!ranked-check") {
-    //   await msg.reply("Perfoming ranked check on everybody!");
-    //   players.forEach(async playerID => {
-    //     const response = await axios.get(formatLeagueAPIQuery(playerID));
-    //     // console.log(response.data);
-    //     let soloQStats, flexStats;
-    //     if (response.data[0].queueType == "RANKED_FLEX_SR") {
-    //       flexStats = response.data[0];
-    //       soloQStats = response.data[1];
-    //     } else {
-    //       flexStats = response.data[1];
-    //       soloQStats = response.data[0];
-    //     }
-    //     const { summonerName } = response.data[0];
-    //     const embed = new MessageEmbed()
-    //       .setTitle(summonerName)
-    //       .addField(
-    //         "Solo/Duo",
-    //         `Tier: ${soloQStats.tier}-${soloQStats.rank} ${
-    //           soloQStats.miniSeries ? `(${soloQStats.miniSeries.progress})` : ""
-    //         }\nWinRate: ${Math.floor(
-    //           (soloQStats.wins / (soloQStats.wins + soloQStats.losses)) * 100
-    //         )}%`
-    //       )
-    //       .addField(
-    //         "Flex",
-    //         `Tier: ${flexStats.tier}-${flexStats.rank} ${
-    //           flexStats.miniSeries ? `(${flexStats.miniSeries.progress})` : ""
-    //         }\nWinRate: ${Math.floor(
-    //           (flexStats.wins / (flexStats.wins + flexStats.losses)) * 100
-    //         )}%`
-    //       )
-    //       .setColor("#dddddd");
-
-    //     await msg.channel.send(embed);
-    //   });
-    // }
-    // else if(msg.content === ){
-
-    // }
   }
 };
